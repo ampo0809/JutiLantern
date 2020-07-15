547a4d4abd61f4cba35a511f97a7a9de40222ecf
Nacho Ampuero <okihand@hotmail.com>
2020-07-15 16:38:49 +0200

    Initial Commit

---
+++ JutiLantern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JutiLantern
+//
+//  Created by Nacho Ampuero on 15.07.20.
+//  Copyright © 2020 Nacho Ampuero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
